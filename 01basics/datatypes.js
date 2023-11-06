fd503aa557a3cb4fcb9c1276e5db85e37d626c49
YashVar304 <113633718+YashVar304@users.noreply.github.com>
2023-11-06 16:22:15 +0000

    datatypes

---
+++ 01basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//code will be treated as newer version
+
+//alert(3+3)
+
+//datatype
+// number=> 2 to power 53
+// bigInt=> unlimited range
+// string
+// boolean
+// null=> standalone value
+// undefined 
+// symbol => used to define uniquely
+
+
+//object
+let age = 3
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
--- 01basics/variable.js
+++ 01basics/variable.js
@@ -3,7 +3,8 @@ let accountEmail="yashvarshney304@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState
-
+/*prefer not to use var
+because it has problem with block scope and functional scope */
 //accountId = 3
 accountEmail = "yashnati304@gmail.com"
 accountPassword = "123456789"
