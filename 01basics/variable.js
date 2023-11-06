8bed182b13a423b4408aaaf9bc45da1e9f34d725
YashVar304 <113633718+YashVar304@users.noreply.github.com>
2023-11-06 15:58:54 +0000

    variable

---
+++ 01basics/variable.js
@@ -0,0 +1,12 @@
+const accountId=3456
+let accountEmail="yashvarshney304@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 3
+accountEmail = "yashnati304@gmail.com"
+accountPassword = "123456789"
+accountCity = "bahjoi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
